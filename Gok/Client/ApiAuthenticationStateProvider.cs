a35188676fb9c7cffa72bfb9a1b312f0aa243281
Joren Schelkens <52953553+JorenSchelkens@users.noreply.github.com>
2019-09-17 21:20:19 +0200

    Bug fixed

--- Gok/Client/ApiAuthenticationStateProvider.cs
+++ Gok/Client/ApiAuthenticationStateProvider.cs
@@ -1,5 +1,6 @@
 ﻿using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.VisualStudio.Shell;
 using System;
 using System.Collections.Generic;
@@ -22,31 +23,31 @@ namespace Gok.Client
             _httpClient = httpClient;
             _localStorage = localStorage;
         }
-        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        public override async Task<Microsoft.AspNetCore.Components.Authorization.AuthenticationState> GetAuthenticationStateAsync()
         {
             var savedToken = await _localStorage.GetItemAsync<string>("authToken");
 
             if (string.IsNullOrWhiteSpace(savedToken))
             {
-                return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+                return new Microsoft.AspNetCore.Components.Authorization.AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
             }
 
             _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", savedToken);
 
-            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(ParseClaimsFromJwt(savedToken), "jwt")));
+            return new Microsoft.AspNetCore.Components.Authorization.AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(ParseClaimsFromJwt(savedToken), "jwt")));
         }
 
         public void MarkUserAsAuthenticated(string email)
         {
             var authenticatedUser = new ClaimsPrincipal(new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, email) }, "apiauth"));
-            var authState = Task.FromResult(new AuthenticationState(authenticatedUser));
+            var authState = Task.FromResult(new Microsoft.AspNetCore.Components.Authorization.AuthenticationState(authenticatedUser));
             NotifyAuthenticationStateChanged(authState);
         }
 
         public void MarkUserAsLoggedOut()
         {
             var anonymousUser = new ClaimsPrincipal(new ClaimsIdentity());
-            var authState = Task.FromResult(new AuthenticationState(anonymousUser));
+            var authState = Task.FromResult(new Microsoft.AspNetCore.Components.Authorization.AuthenticationState(anonymousUser));
             NotifyAuthenticationStateChanged(authState);
         }
 
