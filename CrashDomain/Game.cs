acd6e3b6192b1ffae44a7a639d9178e1485c8a59
Maxim2012-dev <55281306+Maxim2012-dev@users.noreply.github.com>
2019-10-10 15:35:32 +0200

    begin van Crashgame

---
+++ CrashDomain/Game.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace CrashDomain
+{
+    public class Game
+    {
+
+        public double multiplier { get; set; }
+        public int muntenIngezet { get; set; }
+        public double autoCashOut { get; set; }
+        public bool spelGedaan { get; set; } = false;
+        public Timer timer;
+
+        public Game(double multiplier, int muntenIngezet, double autoCashOut)
+        {
+            this.multiplier = multiplier;
+            this.muntenIngezet = muntenIngezet;
+            this.autoCashOut = autoCashOut;
+        }
+
+        public void VerhoogMultiplier()
+        {
+            var random = new Random();
+            var totalMultiplier = random.NextDouble();
+
+            timer = new Timer(new TimerCallback(_ =>
+            {
+
+            }), null, 1000, 1000);
+        }
+
+        public bool IsGecrasht(double random)
+        {
+            if(multiplier == random)
+            {
+                return true;
+            }
+            return false;
+
+        }
+  
+    }
+}
