8f32c5017a2e59a3c61d6a3e0e54142a5e5cce98
Izombiefyyou2 <55281424+Izombiefyyou2@users.noreply.github.com>
2019-10-10 15:32:56 +0200

    versie 2.0 van Blackjack

--- BlackJackDomain/BlackjackTest.cs
+++ BlackJackDomain/BlackjackTest.cs
@@ -2,9 +2,23 @@
 
 namespace BlackJackDomain
 {
+
     public class Blackjack
     {
         Random rand = new Random();
+        private Boolean gewonnen{ get; set; }
+        private char kaart { get; set; }
+        private double inzet{ get; set; }
+
+
+        public Blackjack(Boolean gewonnen, char kaart, double inzet)
+        {
+            this.gewonnen = gewonnen;
+            this.kaart = kaart;
+            this.inzet = inzet;
+        }
+        
+        
 
     }
 }
