b3196f2d562636b06ba1342aa3ca38e56f0002aa
Joren Schelkens <52953553+JorenSchelkens@users.noreply.github.com>
2019-10-11 16:11:26 +0200

    Update Encryption.cs

--- DefaultClasses/Encryption.cs
+++ DefaultClasses/Encryption.cs
@@ -1,43 +1,23 @@
-﻿using System;
-using System.IO;
-using System.Security.Cryptography;
+﻿using System.Security.Cryptography;
 using System.Text;
 
 namespace DefaultClasses
 {
     public class Encryption
     {
-        protected static string Key = "64E8BC0D7B414A25B143FD9E3841BC4B";
-
         public static string EncryptString(string text)
         {
-            var key = Encoding.UTF8.GetBytes(Key);
+            MD5 md5 = new MD5CryptoServiceProvider();
+            md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(text));
 
-            using (var aesAlg = Aes.Create())
-            {
-                using (var encryptor = aesAlg.CreateEncryptor(key, aesAlg.IV))
-                {
-                    using (var msEncrypt = new MemoryStream())
-                    {
-                        using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
-                        using (var swEncrypt = new StreamWriter(csEncrypt))
-                        {
-                            swEncrypt.Write(text);
-                        }
+            byte[] result = md5.Hash;
 
-                        var iv = aesAlg.IV;
-
-                        var decryptedContent = msEncrypt.ToArray();
-
-                        var result = new byte[iv.Length + decryptedContent.Length];
-
-                        Buffer.BlockCopy(iv, 0, result, 0, iv.Length);
-                        Buffer.BlockCopy(decryptedContent, 0, result, iv.Length, decryptedContent.Length);
-
-                        return Convert.ToBase64String(result);
-                    }
-                }
+            StringBuilder strBuilder = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            { 
+                strBuilder.Append(result[i].ToString("x2"));
             }
+            return strBuilder.ToString();
         }
 
     }
