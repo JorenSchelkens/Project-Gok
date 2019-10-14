f0ebee1dab32b52e6240f2aa61a6bbcefd7715d8
IgnaceGeys <55281405+IgnaceGeys@users.noreply.github.com>
2019-10-14 15:26:04 +0200

    Kaarten
    Code voor kaarten te laten zien

---
+++ DefaultClasses/UrlGenerator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefaultClasses
+{
+    public static class UrlGenerator
+    {
+        public static List<string> GenerateUrls()
+        {
+            List<string> cards = new List<string>();
+
+            string baseUrl = "img/Kaarten/";
+            string soort = "";
+            string index;
+            string url;
+
+            for (int i = 0; i < 4; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        soort = "C";
+                        break;
+                    case 1:
+                        soort = "D";
+                        break;
+                    case 2:
+                        soort = "H";
+                        break;
+                    case 3:
+                        soort = "S";
+                        break;
+                }
+
+                for (int j = 1; j <= 13; j++)
+                {
+                    switch (j)
+                    {
+                        case 1:
+                            index = "A";
+                            break;
+                        case 11:
+                            index = "J";
+                            break;
+                        case 12:
+                            index = "Q";
+                            break;
+                        case 13:
+                            index = "K";
+                            break;
+                        default:
+                            index = j.ToString();
+                            break;
+                    }
+
+                    url = baseUrl + soort + index + ".png";
+                    cards.Add(url);
+
+                }
+
+            }
+
+            return cards;
+        }
+
+    }
+}
