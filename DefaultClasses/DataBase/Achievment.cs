aa4d581238b1c1721ad8454cbe9a73ddffdfe67e
koenhellemans <55281372+koenhellemans@users.noreply.github.com>
2019-11-29 12:03:52 +0100

    Achievement

---
+++ DefaultClasses/DataBase/Achievment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DefaultClasses.DataBase
+{
+    public class Achievment
+    {
+        public int Nummer { get; set; }
+        public string AchievementOmschrijving { get; set; }
+        public int Spelernummer { get; set; }
+        public int AantalGewonnenGames { get; set; }
+        public int Beloning { get; set; }
+
+        public event EventHandler StateChanged;
+
+        public void Refresh()
+        {
+            this.StateHasChanged();
+        }
+
+        private void StateHasChanged()
+        {
+            this.StateChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
